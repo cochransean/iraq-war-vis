650caff4b88e32ba66b3b4103c1e5528ca724999
Theodore Aronson <tha779@mail.harvard.edu>
2016-04-08 14:59:44 -0400

    tested force layout with esoc data

--- js/force_demo_js.js
+++ js/force_demo_js.js
@@ -10,43 +10,40 @@ var force = d3.layout.force()
     .size([width,height])
     .linkDistance(50);
 
-d3.csv("data/esoc-iraq-v3_ibc.csv",function(error, links){
-    console.log(links);
+d3.csv("data/esoc-iraq-v3_ethnicity.csv",function(error, links){
+    //console.log(links);
+
     if (error) throw error;
 
     var nodesByName = {};
 
     links.forEach(function(link){
-        link.source = nodeByName(link.source);
-        link.target = nodeByName(link.target);
+        link.district = nodeByName(link.district);
+        link.sunni_pop_CIA_1978 = +link.sunni_pop_CIA_1978;
     });
-
+    
     var nodes = d3.values(nodesByName);
 
-    var link = svg.selectAll(".link")
-        .data(links)
-        .enter()
-        .append("line")
-        .attr("class","link");
-
     var node = svg.selectAll(".node")
-        .data(nodes)
+        .data(links)
         .enter().append("circle")
         .attr("class", "node")
         .attr("r", 4.5)
-        .call(force.drag);
+        .call(force.drag)
+        .attr("fill", function(d){
+            if (d.sunni_pop_CIA_1978 > 1000)
+                return ("blue");
+            else
+                return ("red");
+        });
+
 
     force
-        .nodes(nodes)
-        .links(links)
+        .nodes(links)
         .on("tick", tick)
         .start();
 
     function tick() {
-        link.attr("x1", function(d) {return d.source.x})
-        .attr("y1", function(d){return d.source.y})
-        .attr("x2", function(d){return d.target.x})
-        .attr("y2", function(d){return d.target.y});
 
         node.attr("cx", function(d){return d.x})
             .attr("cy", function(d){return d.y});
@@ -56,5 +53,6 @@ d3.csv("data/esoc-iraq-v3_ibc.csv",function(error, links){
     function nodeByName(name){
         return nodesByName[name] || (nodesByName[name] = {name: name});
     }
+
 });
 
