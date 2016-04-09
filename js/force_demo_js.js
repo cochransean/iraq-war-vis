214334b752687356ce9396ab517bcac0a237a750
Theodore Aronson <tha779@mail.harvard.edu>
2016-04-09 12:22:01 -0400

    added button

--- js/force_demo_js.js
+++ js/force_demo_js.js
@@ -10,38 +10,52 @@ var force = d3.layout.force()
     .size([width,height])
     .linkDistance(50);
 
+var data;
+
 d3.csv("data/esoc-iraq-v3_sigact-district-year.csv",function(error, links){
 
     if (error) throw error;
 
-    links.forEach(function(link){
+    links.filter(function(link){
         link.suicide = +link.suicide;
         link.year = +link.year;
     });
 
-    links.filter(function(link){
-        return link.year == 2004;
-    });
     console.log(links);
 
+    data = links;
+
+    updateVisualization();
+    $('#button').click();
+
+});
+
+function updateVisualization(){
+
+    var ranking = d3.select("#button").property("value");
+
     var node = svg.selectAll(".node")
-        .data(links)
+        .data(data)
         .enter().append("circle")
+        .filter(function(d){
+            return d.year == ranking;
+        })
         .attr("class", "node")
         .attr("r", 5)
-        .attr("r", function(d){
-            return d.suicide
+        .attr("r",function(d){
+            return d.suicide;
         })
         .call(force.drag);
 
     force
-        .nodes(links)
+        .nodes(data)
         .on("tick", tick)
         .start();
+
 
     function tick() {
         node.attr("cx", function(d){return d.x})
             .attr("cy", function(d){return d.y});
     }
+}
 
-});
