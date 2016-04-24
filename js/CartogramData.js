29153e60993ba4223ec9a7a7ac31ed4dea1031c8
MaxNorz <maximilian_norz@hks17.harvard.edu>
2016-04-24 17:27:06 -0500

    cleaned code in cartogramdata.js

--- js/CartogramData.js
+++ js/CartogramData.js
@@ -1,56 +1,37 @@
 
-var field = {name: "Population Estimate", id: "popest", key: "Casualties"};
-var colors = colorbrewer.RdYlBu[5];
+// TODO embed it nicely in DOM
+// TODO now it automatically updates and shows casualties, but it would obviously be much nicer if the user
+// TODO could switch between two views: the original US map and the cartogram
+// TODO see CartogramDataOriginal.js for the original code (from http://prag.ma/code/d3-cartogram/)
 
-var body = d3.select("body");
-var stat = d3.select("#status");
-
-var map = d3.select("#map");
-var zoom = d3.behavior.zoom()
-    .translate([-38, 32])
-    .scale(.94)
-    .scaleExtent([0.5, 10.0])
-    .on("zoom", updateZoom);
-var layer = map.append("g")
-    .attr("id", "layer");
-var states = layer.append("g")
+// create group for state paths
+var states = d3.select("#map")
+    .append("g")
     .attr("id", "states")
     .selectAll("path");
 
-// map.call(zoom);
-updateZoom();
-
-function updateZoom() {
-    var scale = zoom.scale();
-    layer.attr("transform",
-        "translate(" + zoom.translate() + ") " +
-        "scale(" + [scale, scale] + ")");
-}
-
-var proj = d3.geo.albersUsa();
-var topology;
-var geometries;
-var rawData;
-var dataById = {};
-var carto = d3.cartogram()
-    .projection(proj)
+// set up projection and variable to call cartogram
+var projection = d3.geo.albersUsa();
+var cartogram = d3.cartogram()
+    .projection(projection)
     .properties(function(d) {
-        return dataById[d.id];
-    })
-    .value(function(d) {
-        return +d.properties[field];
+        // TODO I don't understand why id is added to d.id
+        return stateCasualtiesObject[d.id];
     });
 
-window.onhashchange = function() {
-    parseHash();
-};
+// create variables for geo and casualty data
+var topology;
+var geometries;
+var casualties;
+var stateCasualtiesObject = {};
 
+// load data
 d3.json("data/us-states.topojson", function(topo) {
     topology = topo;
     geometries = topology.objects.states.geometries;
     d3.csv("data/CasualtiesUSStates.csv", function(data) {
-        rawData = data;
-        dataById = d3.nest()
+        casualties = data;
+        stateCasualtiesObject = d3.nest()
             .key(function(d) { return d.Name; })
             .rollup(function(d) { return d[0]; })
             .map(data);
@@ -59,10 +40,13 @@ d3.json("data/us-states.topojson", function(topo) {
 });
 
 function init() {
-    var features = carto.features(topology, geometries),
-        path = d3.geo.path()
-            .projection(proj);
 
+    // add features to cartogram
+    var features = cartogram.features(topology, geometries);
+    var path = d3.geo.path()
+        .projection(projection);
+
+    // create initial map
     states = states.data(features)
         .enter()
         .append("path")
@@ -70,96 +54,51 @@ function init() {
         .attr("id", function(d) {
             return d.properties.Name;
         })
-        .attr("fill", "#fafafa")
+        .attr("fill", "red")
         .attr("d", path);
 
+    // add tooltips for casualty numbers
     states.append("title");
 
-    parseHash();
+    update();
 }
 
 function update() {
-    var start = Date.now();
-    body.classed("updating", true);
 
-    var key = field.key,
-        fmt = (typeof field.format === "function")
-            ? field.format
-            : d3.format(field.format || ","),
-        value = function(d) {
-            return +d.properties[key];
-        },
-        values = states.data()
-            .map(value)
-            .filter(function(n) {
-                return !isNaN(n);
-            })
-            .sort(d3.ascending),
-        lo = values[0],
-        hi = values[values.length - 1];
+    // create array and sort casualty numbers
+    var value = function(d) {
+            return +d.properties["Casualties"];
+        };
 
-    var color = d3.scale.linear()
-        .range(colors)
-        .domain(lo < 0
-            ? [lo, 0, hi]
-            : [lo, d3.mean(values), hi]);
+    // use quartiles (calculated in excel) as thresholds
+    var color = d3.scale.threshold()
+        .domain([25, 62, 88, 500])
+        .range(["#fee5d9","#fcae91","#fb6a4a","#cb181d"]);
 
-    // normalize the scale to positive numbers
-    var scale = d3.scale.linear()
-        .domain([lo, hi])
-        .range([1, 1000]);
-
-    // tell the cartogram to use the scaled values
-    carto.value(function(d) {
-        return scale(value(d));
+    // tell the cartogram which values to use
+    cartogram.value(function(d) {
+        return value(d);
     });
 
-    // generate the new features, pre-projected
-    var features = carto(topology, geometries).features;
+    // generate the new features (pre-projected)
+    var features = cartogram(topology, geometries).features;
 
     // update the data
     states.data(features)
         .select("title")
         .text(function(d) {
-            return [d.properties.Name, fmt(value(d))].join(": ");
+            // TODO how do you call them? I don't think soldiers is correct.
+            return value(d) + " US soldiers that died came from " + [d.properties.Name];
         });
 
+    // set transition 
+    // TODO makes little sense with just one view
     states.transition()
-        .duration(750)
+        .duration(2000)
         .ease("linear")
         .attr("fill", function(d) {
             return color(value(d));
         })
-        .attr("d", carto.path);
+        .attr("d", cartogram.path);
 
-    var delta = (Date.now() - start) / 1000;
-    stat.text(["calculated in", delta.toFixed(1), "seconds"].join(" "));
-    body.classed("updating", false);
-}
-
-var deferredUpdate = (function() {
-    var timeout;
-    return function() {
-        var args = arguments;
-        clearTimeout(timeout);
-        stat.text("calculating...");
-        return timeout = setTimeout(function() {
-            update.apply(null, arguments);
-        }, 10);
-    };
-})();
-
-var hashish = d3.selectAll("a.hashish")
-    .datum(function() {
-        return this.href;
-    });
-
-function parseHash() {
-
-    deferredUpdate();
-    location.replace("#" + field.id);
-
-    hashish.attr("href", function(href) {
-        return href + location.hash;
-    });
 }
